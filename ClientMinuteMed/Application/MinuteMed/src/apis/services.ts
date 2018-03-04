ec3ce5c7123021b7a6a5d02898e8bf674135c693
Android Project <zecchini.aymeric@gmail.com>
2018-03-04 15:50:14 +0100

    [Aymeric] - Changement répertoire API

--- ClientMinuteMed/Application/MinuteMed/src/apis/services.ts
+++
@@ -1,5 +0,0 @@
-export { AuthenticationRESTEndpointService } from './services/authentication-restendpoint.service';
-export { ArborescenceRESTEndpointService } from './services/arborescence-restendpoint.service';
-export { MedicalRecordsRESTEndpointService } from './services/medical-records-restendpoint.service';
-export { SpecializationsRESTEndpointService } from './services/specializations-restendpoint.service';
-export { StaffRESTEndpointService } from './services/staff-restendpoint.service';
